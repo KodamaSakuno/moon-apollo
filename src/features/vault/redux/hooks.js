679586c5e54792774351a71fd1ddc43350485cc5
beichen <13520648600@qq.com>
2021-01-13 14:56:30 +0800

    merge

--- src/features/vault/redux/hooks.js
+++ src/features/vault/redux/hooks.js
@@ -1,16 +1,128 @@
 import { erc20ABI, LunarModuleAbi } from 'features/configure/abi';
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { enqueueSnackbar } from '../../common/redux/actions';
-import { ethers } from 'ethers';
+import { ethers, utils, BigNumber } from 'ethers';
 
 import { useConnectWallet } from '../../home/redux/hooks';
+import { getBasicsOf } from "../../common/LunarModule";
+import { ReadOnlyProvider } from "../../common/rpcProviders";
 export { useFetchBalances } from './fetchBalances';
 export { useFetchPoolBalances } from './fetchPoolBalances';
 export { useFetchApproval } from './fetchApproval';
 export { useFetchDeposit } from './fetchDeposit';
 export { useFetchWithdraw } from './fetchWithdraw';
 export { useFetchContractApy } from './fetchContractApy';
+
+export function usePoolApy(
+    pool,
+    everyRewardValue,
+    everyStakingValue
+    // pool.itokenDecimals: number,
+    // pool.tokenDecimals: number
+  ) {
+    const [rewardRate, updateRewardRate] = useState(BigNumber.from(0));
+    const [totalStaked, updateTotalStaked] = useState(BigNumber.from(0));
+    const [stats, updateStats] = useState({
+      mission: "",
+    });
+  
+    const contract = useMemo(() => {
+      return new ethers.Contract(
+        pool.earnContractAddress,
+        LunarModuleAbi,
+        ReadOnlyProvider[pool.chainId]
+      );
+    }, [pool]);
+  
+    const update = useCallback(async () => {
+      // rewardRate = reward for every second staking
+      // const rewardRate = await contract.rewardRate();
+      // // totalStaked amounts for this pool
+      // const totalStaked = await contract.totalSupply();
+      // const currentMission = await contract.mission();
+      const { rewardRate, totalSupply, mission } = await getBasicsOf(
+        pool.chainId,
+        pool.earnContractAddress
+      );
+  
+      updateRewardRate(rewardRate);
+      updateTotalStaked(totalSupply);
+      updateStats({
+        mission,
+      });
+    }, [pool.earnContractAddress, pool.chainId]);
+  
+    // let apyForDisplay = '---.--'
+    // if (rewardRate !== '0' || totalStaked !== '0' ) {
+    //     // 365天，24小时，每个小时3600秒
+    //     const yearlyRewardInBNB = Number(rewardRate) * 365 * 24 * 3600
+    //     // 年利润 / 总抵押额。均以BNB为计价单位
+    //     const _apy = (yearlyRewardInBNB / Number(totalStaked))
+    //     apyForDisplay = (_apy * 100).toFixed(2)
+    // }
+  
+    const isPoolStopped = useMemo(() => rewardRate.eq(0), [rewardRate]);
+  
+    const memoizedApy = useMemo(() => {
+      if (pool.itokenDecimals === 0 || pool.tokenDecimals === 0) {
+        return "---.--";
+      }
+      const formattedRewardTokenValue = utils.formatUnits(
+        everyRewardValue,
+        pool.itokenDecimals
+      );
+      const formattedStakingTokenValue = utils.formatUnits(
+        everyStakingValue,
+        pool.tokenDecimals
+      );
+  
+      const formattedRewardRate = utils.formatUnits(
+        rewardRate,
+        pool.itokenDecimals
+      );
+      const formattedtotalStaked = utils.formatUnits(
+        totalStaked,
+        pool.tokenDecimals
+      );
+      // 365天，24小时，每个小时3600秒
+      const yearlyRewardInBNB =
+        Number(formattedRewardRate) *
+        365 *
+        24 *
+        3600 *
+        Number(formattedRewardTokenValue);
+      const totalStakedTokenInBNB =
+        Number(formattedtotalStaked) * Number(formattedStakingTokenValue);
+      let apy = yearlyRewardInBNB / totalStakedTokenInBNB;
+      if (pool.chainId === 56) 
+        apy = apy * 1600; // multiply with 1600 for bnb chain ?
+      const apyForDisplay = (apy * 100).toFixed(2);
+      return apy === Number.POSITIVE_INFINITY ? "---.--" : apyForDisplay;
+    }, [
+      everyRewardValue,
+      everyStakingValue,
+      pool.itokenDecimals,
+      pool.tokenDecimals,
+      rewardRate,
+      totalStaked,
+      pool.chainId,
+    ]);
+    useEffect(() => {
+      if (contract) {
+        update();
+      }
+    }, [contract, update]);
+  
+    return {
+      update,
+      apy: memoizedApy,
+      totalStake: totalStaked,
+      rewardRate: rewardRate,
+      stats,
+      isPoolStopped,
+    };
+  }
 
 export function useApprove(tokenAddress, poolAddress) {
     const { web3, address } = useConnectWallet();
